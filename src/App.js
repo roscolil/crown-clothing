477cf1153407aa3c1b55ab20e8792d8c90b7ccc6
Ross <ross.lillis@gmail.com>
2021-04-12 12:08:39 +1000

    add collection preivew component with shop data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/ShopPage/ShopPage'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" component={HomePage} exact />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
