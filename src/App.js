9bb53f671f26327a2c87565d0975bceb9f0df261
Ross <ross.lillis@gmail.com>
2021-04-13 10:58:22 +1000

    implement firebase utils incl ability to store auth users into firestore db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage'
 import Header from './components/Header/Header';
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -19,8 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({ currentUser: userAuth })
     })
   }
 
--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SignInAndSignUp.scss";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
